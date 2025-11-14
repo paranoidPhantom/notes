518193e3aa478304e731837be85f52882f6cef35
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-11-14 20:36:40 +0300

    fix(ui): mobile header fixed

--- app/layouts/default.vue
+++ app/layouts/default.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="max-w-[1000px] w-full mx-auto">
         <header
-            class="flex md:hidden mx-8 my-4 ml-auto mb-0 p-4 ring ring-accent rounded-xl w-fit"
+            class="right-4 top-4 z-10 backdrop-blur-xl flex fixed md:hidden mx-8 my-4 mb-0 p-4 ring ring-accent rounded-xl w-fit"
         >
             <SidebarTrigger />
         </header>
