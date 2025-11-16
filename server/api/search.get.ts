b1c4ae8240510090e1ec0b46544ccc2458990637
Andrei Hudalla <46633092+paranoidPhantom@users.noreply.github.com>
2025-11-16 22:14:46 +0300

    refactor(ui): massively improve search

--- server/api/search.get.ts
+++ server/api/search.get.ts
@@ -1,9 +1,11 @@
 import Fuse from "fuse.js";
+import { findPageBreadcrumb } from "@nuxt/content/utils";
 
 export default defineEventHandler(async (event) => {
     const { query } = getQuery(event);
 
     const content = await queryCollectionSearchSections(event, "content");
+    const navigation = await queryCollectionNavigation(event, "content");
 
     const fuse = new Fuse(content, {
         keys: ["content", "title"],
@@ -13,5 +15,16 @@ export default defineEventHandler(async (event) => {
 
     const result = fuse.search(query as string);
 
-    return result.slice(0, 5);
+    return result.slice(0, 5).map((r) => ({
+        ...r,
+        item: {
+            ...r.item,
+            titles: [
+                ...findPageBreadcrumb(navigation, r.item.id).map(
+                    (p) => p.title
+                ),
+                ...r.item.titles,
+            ],
+        },
+    }));
 });
